5963617b32edcc9fb14f51a78d8b3b396a090e5f
PANDISELVI S <selvisankar20209@gmail.com>
2024-12-04 16:58:25 +0530

    Add files via upload
    A simple website for my portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Coding Teacher","Web Development Tutor","Job Seeker","Aspiring Software Developer"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000
+})
